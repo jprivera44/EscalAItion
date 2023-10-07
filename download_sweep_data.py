65d472e6349c8ef5c4ce48745a14382f54451a4b
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-10-07 12:35:35 -0700

    Code for batch downloading sweep data

--- download_sweep_data.py
+++ download_sweep_data.py
@@ -14,23 +14,62 @@ OUTPUT_FOLDER_VARIABLES = "results/variables_v2"
 
 
 def main():
-    """Main function."""
+    r"""
+    Main function to download the data.
 
-    api = wandb.Api()
+    Note: From a sweeps page, you can run the following JavaScript console command to print the IDs and names of the runs in CSV format:
+    ```
+    output = "sweep_id,prefix\n"
+    labels = document.querySelectorAll('a > span > .editable-label')
+    for (let i = 0; i < labels.length; i++) {
+        label = labels[i]
+        sweep_id = label.parentElement.parentElement.href.split("/")[6]
+        output += sweep_id + "," + label.innerHTML + "\n"
+    }
+    console.log(output)
+    ```
+    You may then want to modify it to remove bad sweeps or have different prefixes (e.g. without version numbers).
+    """
 
     # Get user input for the sweep ID
-    sweep_id = input("Enter the sweep ID: ")
-    # sweep_id = "kckc8ckq"
+    sweep_id_and_maybe_prefix = input(
+        "Enter either:\n\t1. The sweep ID\n\t2. The sweep ID and optionally a space then the prefix for donloaded files\n\t3. A path to a CSV text file with 'sweep_id' and 'prefix' columns\n"
+    )
+    if sweep_id_and_maybe_prefix.endswith(".csv"):
+        df = pd.read_csv(sweep_id_and_maybe_prefix)
+        for _, row in tqdm(df.iterrows(), total=len(df), desc="🧹 Sweeps"):
+            sweep_id = row["sweep_id"]
+            prefix = row["prefix"]
+            download_sweep_data(sweep_id, prefix)
+        return
+    sweep_id_and_maybe_prefix = sweep_id_and_maybe_prefix.strip().split(" ", maxsplit=1)
+    if len(sweep_id_and_maybe_prefix) == 1:
+        sweep_id = sweep_id_and_maybe_prefix[0]
+        prefix = None
+    else:
+        assert len(sweep_id_and_maybe_prefix) == 2
+        sweep_id = sweep_id_and_maybe_prefix[0].strip()
+        prefix = sweep_id_and_maybe_prefix[1].strip()
+
+    # Get user input for the prefix they want to use for the downloaded files
+    if prefix is None:
+        prefix = input("Enter the prefix for the downloaded files: ").strip()
+    # prefix = "DEBUG"
+
+    # Download the data
+    download_sweep_data(sweep_id, prefix)
+
+
+def download_sweep_data(sweep_id: str, prefix: str) -> None:
+    """Download the artifacts from the runs in the sweep."""
+    # Initialize W&B API
+    api = wandb.Api()
 
     # Get the sweep
     sweep = api.sweep(f"{constants.WANDB_PROJECT}/{sweep_id}")
 
     # Get the runs in the sweep
     runs = list(sweep.runs)
-
-    # Get user input for the prefix they want to use for the downloaded files
-    prefix = input("Enter the prefix for the downloaded files: ").strip()
-    # prefix = "DEBUG"
 
     # Download the artifacts from each run
     for run in tqdm(runs):
