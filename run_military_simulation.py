527664218603a63759f830853ab1f0c4fedbd529
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-10-06 17:23:31 -0700

    v4 nations

--- run_military_simulation.py
+++ run_military_simulation.py
@@ -48,7 +48,7 @@ def main():
     parser.add_argument(
         "--nations_config_filepath",
         type=str,
-        default="nations_configs/nations_v3.csv",
+        default="nations_configs/nations_v4.csv",
     )
     parser.add_argument(
         "--action_config_filepath",
