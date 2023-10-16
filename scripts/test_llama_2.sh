af594121e08e52bb232edcbeaaa2867e7e650946
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-10-16 15:26:06 -0700

    Llama-2 base script

--- scripts/test_llama_2.sh
+++ scripts/test_llama_2.sh
@@ -1,1 +1,1 @@
-python run_military_simulation.py --disable_wandb --actions_in_prompts False --nation_model Yukang/Llama-2-70b-longlora-32k
+python run_military_simulation.py --disable_wandb --actions_in_prompts False --rope_scaling_dynamic 2.0 --nation_model meta-llama/Llama-2-70b-hf
