c6bf5eb1fb872d53e51361fb6fa9e44f52a39502
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-10-15 17:00:47 -0700

    Bump max tokens for HF models

--- backends.py
+++ backends.py
@@ -57,7 +57,7 @@ class HuggingFaceCausalLMBackend(LanguageModelBackend):
         super().__init__()
         self.model_name = model_name
         self.device = device
-        self.max_tokens = 1000  # TODO does this upperbound include the input prompt?
+        self.max_tokens = 2000
 
         if quantization == 4:
             fourbit = True
