5dc8f94cb4e2cdabeba89d927b340d7fe63bd24f
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-12-19 22:30:16 -0800

    fix legend

--- charts/action_count_graphs_final.py
+++ charts/action_count_graphs_final.py
@@ -37,7 +37,7 @@ OUTPUT_DIR_ACTIONS_OVER_TIME = "./actions_over_time"
 OUTPUT_DIR_SEVERITY_BY_MODEL = "./severity_by_model"
 OUTPUT_DIR_DISTRIBUTIONS_ALL_ACTIONS = "./distributions_all_actions"
 
-PLOT_NUMBER_TO_CREATE = 2  # -1 to create all plots
+PLOT_NUMBER_TO_CREATE = 5  # -1 to create all plots
 
 LABEL_MAX_LENGTH = 26
 
@@ -723,9 +723,9 @@ def main() -> None:
             title = f"Severity of Actions by Model ({scenario} Scenario)"
             plt.title(f"{scenario} Scenario")
             plt.legend(
-                title="Model",
-                loc="best",
-                framealpha=0.5,
+                # title="Model",
+                loc="upper right",
+                # framealpha=0.5,
                 borderaxespad=0.0,
                 handletextpad=0.1,
                 labelspacing=0.25,
